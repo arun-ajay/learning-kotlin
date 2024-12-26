56804bb486231e0c4e62f61f44031bd6056a25d2
arun-ajay <arunajay42795@gmail.com>
2024-12-26 16:32:54 -0500

    clear

--- settings.gradle.kts
+++
@@ -1,2 +0,0 @@
-rootProject.name =
-  "learning-kotlin-in-action"
--- src/main/kotlin/main.kt
+++
@@ -1,5 +0,0 @@
-data class Person(val name: String, val age: Int? = null)
-
-fun main(args: Array<String>) {
-    println("Hello, World!")
-}
